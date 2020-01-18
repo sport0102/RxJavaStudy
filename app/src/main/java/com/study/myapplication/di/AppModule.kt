0c4f0940746eb8b324fe478f99990e7c107c6e7c
Aiden <sunminlee89@gmail.com>
2020-01-19 01:11:16 +0900

    소스 수정

--- app/src/main/java/com/study/myapplication/di/AppModule.kt
+++ app/src/main/java/com/study/myapplication/di/AppModule.kt
@@ -1,9 +1,15 @@
 package com.study.myapplication.di
 
-import com.study.myapplication.feature.CompareCoinViewModel
+import com.study.myapplication.feature.compare.CompareCoinViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 fun getAppModule() = module {
-    viewModel { CompareCoinViewModel(get(), get(), get()) }
+    viewModel {
+        CompareCoinViewModel(
+            get(),
+            get(),
+            get()
+        )
+    }
 }
