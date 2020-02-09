6861f3007e7b300e5c45c2307d78912b7e27a2d8
Aiden <sunminlee89@gmail.com>
2020-02-09 16:01:17 +0900

    non-null 하게 수정

--- app/src/main/java/com/study/myapplication/presentation/compare/model/CompareCoinInfo.kt
+++ app/src/main/java/com/study/myapplication/presentation/compare/model/CompareCoinInfo.kt
@@ -1,10 +1,10 @@
 package com.study.myapplication.presentation.compare.model
 
 data class CompareCoinInfo(
-    var coinName: String? = null,
-    var coinonePrice: String? = null,
-    var upbitPrice: String? = null,
-    var bithumbPrice: String? = null,
-    var recommendMarket: String? = null,
-    var differRatio: String? = null
+    var coinName: String = "",
+    var coinonePrice: String = "",
+    var upbitPrice: String = "",
+    var bithumbPrice: String = "",
+    var recommendMarket: String = "",
+    var differRatio: String = ""
 )
