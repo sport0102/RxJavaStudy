ad79f3d96a5416afd619a5b40598a15510790b19
Aiden <sunminlee89@gmail.com>
2020-02-09 15:06:42 +0900

    하드코딩 대신 변수로 Url 관리하게 수정

--- app/src/main/java/com/study/myapplication/MainApplication.kt
+++ app/src/main/java/com/study/myapplication/MainApplication.kt
@@ -1,21 +1,25 @@
 package com.study.myapplication
 
 import android.app.Application
-import com.study.myapplication.presentation.di.getAppModule
 import com.study.myapplication.data.di.getNetworkModule
 import com.study.myapplication.data.di.getRepositoryModule
 import com.study.myapplication.domain.di.getUseCaseModule
+import com.study.myapplication.presentation.di.getAppModule
 
 class MainApplication : Application() {
+
+    private val upbitUrl = "https://api.upbit.com"
+    private val bithumbUrl = "https://api.bithumb.com"
+    private val coinOneUrl = "https://api.coinone.co.kr"
 
     override fun onCreate() {
         super.onCreate()
         setupKoin(
             this,
             getNetworkModule(
-                "https://api.upbit.com",
-                "https://api.bithumb.com",
-                "https://api.coinone.co.kr"
+                upbitUrl,
+                bithumbUrl,
+                coinOneUrl
             ),
             getAppModule(),
             getRepositoryModule(),
